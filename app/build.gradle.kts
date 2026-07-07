6dd7a708a7d90b313cf55040a1cfabd0c6c6882f
subinbabusamueldeveloper <subin.babu.samuel@gmail.com>
2026-07-07 23:37:07 +0100

    add application version as 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 33
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100000
+        versionName = "1.0.0"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
